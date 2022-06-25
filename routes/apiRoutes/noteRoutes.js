bfabe0d45d8fc24132ffd3d5c39a42d2777a964b
robsquaadd <102495137+robsquaadd@users.noreply.github.com>
2022-06-24 21:06:17 -0400

    added ability to add and delete notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,8 @@
 const path = require("path");
+const fs = require("fs");
 const router = require("express").Router();
 const notes = require("../../db/db.json");
+const { v4: uuid } = require("uuid");
 
 router.get("/notes", (req, res) => {
   let results = notes;
@@ -14,6 +16,32 @@ router.get("/notes/:id", (req, res) => {
   } else {
     res.sendStatus(404);
   }
+});
+
+router.post("/notes", (req, res) => {
+  req.body.id = uuid();
+  let notesArray = notes;
+  notesArray.push(req.body);
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(notesArray, null, 2)
+  );
+  res.json(notesArray);
+});
+
+router.delete("/notes/:id", (req, res) => {
+  let id = req.params.id;
+  let notesArray = notes;
+  for (i = 0; i < notesArray.length; i++) {
+    if (notesArray[i].id === id) {
+      notesArray.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, "../../db/db.json"),
+        JSON.stringify(notesArray, null, 2)
+      );
+    }
+  }
+  res.json(notesArray);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
